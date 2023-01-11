f74901917fde1aea7f29c8dd1c34a2f04dec8a1b
kichis <nk.wisteira2@gmail.com>
2023-01-11 23:54:50 +0800

    画像最適化のライブラリを導入

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = import('next-optimized-images');
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+    optimizeImagesInDev: true,
+    handleImages: ['jpeg', 'png']
+  }],
+
+  // your other plugins here
+
+], nextConfig);
