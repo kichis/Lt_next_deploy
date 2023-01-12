fa6691b478caa12088fe5eb1c9153cd9d719a059
kichis <nk.wisteira2@gmail.com>
2023-01-13 01:16:02 +0800

    yarn build追記とnext-optimized-imagesの導入直後の状態に戻した

--- next.config.js
+++ next.config.js
@@ -3,16 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = import('next-optimized-images');
-
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-    optimizeImagesInDev: true,
-    handleImages: ['jpeg', 'png']
-  }],
-
-  // your other plugins here
-
-], nextConfig);
+module.exports = nextConfig
